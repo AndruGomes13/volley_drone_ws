0375c444360873a26a30dca0c2f1a77ed1e1d730
André Gomes <agoncalve@ethz.ch>
2025-07-31 21:06:12 +0000

    Added tmux for policy onboard launch

---
+++ tmux_script/run_onboard_aim_policy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SESSION="run_onboard"
+WORKDIR="$HOME/catkin_ws"
+ROS_SETUP="source $HOME/catkin_ws/devel/setup.sh"
+INIT_SETUP="$ROS_SETUP && cd $WORKDIR  && set_ros_ip && clear"
+
+# Create base session and pane 0.0
+tmux new-session -d -s $SESSION -n main -c "$WORKDIR"
+
+# Split layout: horizontal, then verticals to make 4 panes
+tmux split-window -h -t $SESSION:0.0 -c "$WORKDIR"    # Pane 0.1
+tmux split-window -v -t $SESSION:0.0 -c "$WORKDIR"    # Pane 0.2
+tmux split-window -v -t $SESSION:0.1 -c "$WORKDIR"    # Pane 0.3
+
+# Now send commands
+tmux send-keys -t $SESSION:0.0 "$INIT_SETUP" C-m
+tmux send-keys -t $SESSION:0.0 "roscore"
+
+tmux send-keys -t $SESSION:0.1 "$INIT_SETUP" C-m
+tmux send-keys -t $SESSION:0.1 "roslaunch motion_capture_ros mc_drone_aim.launch" # To figure out
+
+tmux send-keys -t $SESSION:0.2 "$INIT_SETUP" C-m
+tmux send-keys -t $SESSION:0.2 "roslaunch agiros volley_policy_quadrotor_onboard_betaflight.launch quad_name:=volley_drone policy_name:=hover_policy drone_only:=true" # Adjust quad_name as needed
+
+tmux send-keys -t $SESSION:0.3 "$INIT_SETUP" C-m
+
+tmux select-pane -t $SESSION:0.0
+tmux attach -t $SESSION
