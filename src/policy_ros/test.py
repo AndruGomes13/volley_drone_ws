7075a1c2d111c3432c0a7cb8034ac362766dcf6a
André Gomes <agoncalve@ethz.ch>
2025-07-31 20:21:46 +0000

    Changed the policy interface server to use ZMQ

--- src/policy_ros/test.py
+++
@@ -1,7 +0,0 @@
-
-obs = '(1,)'  # Example observation string
-elements = obs.strip("()").split(",")
-elements = list(filter(None, elements))  # Remove empty strings if any
-print(elements)
-a = tuple(map(int, elements))
-print(a)  # Output: (1,)
